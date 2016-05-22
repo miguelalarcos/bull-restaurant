15b5fdeb2dbbd4a473ac7995ecd38a5e59948af4
miguel <miguel.alarcos@gmail.com>
2016-05-22 17:03:04 +0200

    afinando

--- client/ui.rb
+++ client/ui.rb
@@ -23,25 +23,27 @@ class SelectTable < DisplayList
 
   def render
     div do
-      SelectObjectInput(display: 'table', options: state.docs.sort{|a,b| a['table'].to_i <=> b['table'].to_i}, on_change: lambda{|v| params.set_order v['order_id']})
+      SelectObjectInput(display: 'table', options: state.docs.sort{|a,b| a['table'].to_i <=> b['table'].to_i},
+                        on_change: lambda{|v| params.set_order v['order_id']})
     end
   end
 end
 
 class WaiterNotification < DisplayList
   param :waiter
+  param :set_order, type: Proc
 
   before_mount do
     watch_ 'waiter_notifications', params.waiter, []
   end
 
   def render
-    tables = state.docs.inject(Set.new){|acum, x| acum.add([x['order_id'], x['table']])}
+    groups = state.docs.group_by{|x| {order_id: x['order_id'], table: x['table']} }
     div do
-      tables.each do |table|
+      groups.each_pair do |k, lines|
         div do
-          span{table[1]}.on(:click){params.table.value=table[1]}
-          a(href: '#'){'done'}.on(:click){$controller.task('done', table[0])}
+          span{k[:table]}.on(:click){params.set_order k[:order_id]}
+          a(href: '#'){'done'}.on(:click){$controller.task('done', k[:order_id])}
         end
       end
     end
@@ -49,6 +51,7 @@ class WaiterNotification < DisplayList
 end
 
 class ProductMenu < React::Component::Base
+  param :table
   param :order_id
   param :waiter
   param :order_list
@@ -65,9 +68,10 @@ class ProductMenu < React::Component::Base
   def add_product product, price
     line = params.order_list.select{|x| x['name']==product}
     if line.empty?
-      $controller.insert('line', {'order_id'=>params.order_id, 'product'=>product, 'quantity'=>1, 'price'=> price, 'waiter'=>params.waiter})
+      $controller.insert('line', {:table=>params.table, :order_id=>params.order_id, :product=>product, :quantity=>1,
+                                  :price=> price, :waiter=>params.waiter})
     else
-      $controller.update('line', line[0]['id'], {'quantity'=>line[0]['quantity']+1})
+      $controller.update('line', line[0]['id'], {:quantity=>line[0]['quantity']+1})
     end
   end
 
@@ -107,12 +111,13 @@ class WaiterPage < DisplayList
 
   before_mount do
     state.order_id! nil
+    state.table! nil
     watch_ 'watch_table', state.order_id, []
   end
 
   def remove_product line_id, quantity
     if quantity > 1
-      $controller.update('line', line_id, {'quantity'=>quantity-1})
+      $controller.update('line', line_id, {:quantity=>quantity-1})
     else
       $controller.delete('line', line_id)
     end
@@ -120,12 +125,17 @@ class WaiterPage < DisplayList
 
   def render
     div(class: params.show ? '': 'no-display') do
-      WaiterNotification(key: 'waiter_notification', waiter: params.waiter)
+      WaiterNotification(key: 'waiter_notification', waiter: params.waiter,
+                         set_order: lambda{|v| state.order_id! v})
       SelectTable(set_order: lambda{|v| state.order_id! v})
       button{'Nueva mesa'}.on(:click) do
-        $controller.rpc('new_table', params.waiter).then {|response| state.order_id! response}
+        $controller.rpc('new_table', params.waiter).then do |response|
+          state.order_id! response[0]
+          state.table! response[1]
+        end
       end
-      ProducMenu(order_id: state.order_id, waiter: params.waiter, order_list: state.docs.select{|x| x['status'] == 'draft'})
+      ProducMenu(table:state.table, order_id: state.order_id, waiter: params.waiter,
+                 order_list: state.docs.select{|x| x['status'] == 'draft'})
       state.docs.select{|x| x['status'] == 'draft'}.each do |doc|
         div do
           span{doc['product']}
@@ -153,6 +163,7 @@ class WaiterPage < DisplayList
         end
       end
       Total(order_id: state.order_id)
+      button{'Cerrar'}.on(:click){$controller.task('close_order', state.order_id)}
     end
   end
 end
@@ -164,11 +175,15 @@ class KitchenTable < DisplayList
   def render
     div do
       params.docs.each do |doc|
-        div{params.table}
+        div do
+          span{params.table}
+          span{'hecho'}.on(:click){$controller.task('kitchen_done', doc['order_id'])}
+        end
         div do
           span{doc['product']}
           span{' : '}
           span{doc['quantity']}
+          span{'hecho'}.on(:click){$controller.task('kitchen_line_done', doc['id'])}
         end
       end
     end
@@ -204,9 +219,9 @@ class App < React::Component::Base
     div do
       Notification(level: 0)
       HorizontalMenu(page: state.page, set_page: lambda{|v| state.page! v},
-                     options: {'waiter'=>'Camarero', 'kitchen'=>'Cocina'})
-      WaiterPage(key: 'waiter-page', show: state.page == 'waiter', waiter: state.user)
-      KitchenPage(key: 'kitchen-page', show: state.page == 'kitchen')
+                     options: {:waiter=>'Camarero', :kitchen=>'Cocina'})
+      WaiterPage(key: 'waiter-page', show: state.page == :waiter, waiter: state.user)
+      KitchenPage(key: 'kitchen-page', show: state.page == :kitchen)
     end
   end
 end
--- server/main.rb
+++ server/main.rb
@@ -15,15 +15,19 @@ class AppController < BullServerController
     true
   end
 
+  def before_delete_line doc
+    true
+  end
+
   def rpc_products path
     check path, String
-    rmsync $r.table('products').filter('path'=>path)
+    rmsync $r.table('products').filter(:path=>path)
   end
 
   def rpc_new_table waiter
     check waiter, String
-    orders = rmsync $r.table('order').filter('status'=>'opened')
-    tables = orders.collect{|x| x['table']}.sort
+    orders = rmsync $r.table('order').filter(:status=>'opened')
+    tables = orders.collect{|x| x['table'].to_i}.sort
     new_table = 1
     for table in tables
       if new_table != table
@@ -32,43 +36,53 @@ class AppController < BullServerController
       new_table += 1
     end
     ret = rsync $r.table('order').insert(status: 'opened', waiter: waiter, table: new_table.to_s, datetime: Time.now, total: 0.0)
-    ret['generated_keys'][0]
+    [ret['generated_keys'][0], new_table.to_s]
   end
 
   def task_send_to_kitchen order_id
     check order_id, String
-    rsync $r.table('line').filter('order_id'=>order_id, 'status'=>'draft').update('status'=>'kitchen')
+    rsync $r.table('line').filter(:order_id=>order_id, :status=>'draft').update(:status=>'kitchen')
   end
 
-  def task_kitchen_done line
+  def task_kitchen_line_done line
     check line, String
-    rsync $r.table('line').get(line).update('status'=>'kitchen_done')
+    rsync $r.table('line').get(line).update(:status=>'kitchen_done')
+  end
+
+  def task_kitchen_done order_id
+    check order_id, String
+    rsync $r.table('line').filter(:order_id=>order_id, :status=>'kitchen').update(:status=>'kitchen_done')
   end
 
   def task_done order_id
     check order_id, String
-    rsync $r.table('line').filter('order_id'=>order_id, 'status'=>'kitchen_done').update('status'=>'done')
-    lines = rmsync $r.table('line').filter('order_id'=>order_id, 'status'=>'done')
+    rsync $r.table('line').filter(:order_id=>order_id, :status=>'kitchen_done').update(:status=>'done')
+    lines = rmsync $r.table('line').filter(:order_id=>order_id, :status=>'done')
     total = lines.inject(BigDecimal('0')){|sum, n| sum + BigDecimal(n['price'])*BigDecimal(n['quantity'])}
-    rsync $r.table('order').get(order_id).update({'status'=>'closed', 'total'=> total.to_f})
+    rsync $r.table('order').get(order_id).update({:total=> total.to_f})
+  end
+
+  def task_close_order order_id
+    check order_id, String
+    rsync $r.table('order').get(order_id).update({:status=> 'closed'})
   end
 
   def watch_waiter_notifications waiter
     check waiter, String
-    $r.table('line').filter('status'=> 'kitchen_done', 'waiter'=>waiter)
+    $r.table('line').filter(:status=> 'kitchen_done', :waiter=>waiter)
   end
 
   def watch_tables_opened
-    $r.table('order').filter('status'=>'opened')
+    $r.table('order').filter(:status=>'opened')
   end
 
   def watch_table order_id
     check order_id, String
-    $r.table('line').filter('order_id'=>order_id)
+    $r.table('line').filter(:order_id=>order_id)
   end
 
   def watch_kitchen
-    $r.table('line').filter('status'=>'kitchen')
+    $r.table('line').filter(:status=>'kitchen')
   end
 
 end
