81414875af80341b37f6891e3d210b237c73d854
miguel <miguel.alarcos@gmail.com>
2016-06-15 17:58:52 +0200

    first working edit item

--- client/administrative-ui.rb
+++ client/administrative-ui.rb
@@ -42,22 +42,27 @@ class EditItem < React::Component::Base
   param :doc
 
   before_mount do
-    state.display! nil
-    state.price! nil
-    state.backgroundcolor! nil
+    state.display! params.doc['display'] # nil
+    state.price! params.doc['price']
+    state.background_color! params.doc['background-color'] # nil
+    state.color! params.doc['color'] #'black'
+  end
+
+  def item
+    {display: state.display, price: state.price, 'background-color' => state.background_color, color: state.color}
   end
 
   def render
     div do
-      div{StringInput(placeholder: 'display', value: state.display || params.doc['display'], on_change: lambda{|v| state.display! v})}
-      div{FloatInput(placeholder: 'precio', value: state.price || params.doc['price'], on_change: lambda{|v| state.price! v})}
+      div{StringInput(placeholder: 'display', value: state.display, on_change: lambda{|v| state.display! v})}
+      div{FloatInput(placeholder: 'precio', value: state.price, on_change: lambda{|v| state.price! v})}
       div(class: 'red'){'El precio debe ser un número positivo'} if !ValidateItem.validate_price state.price
-      div{input(type: :color, value: params.doc['backgroundcolor']).on(:change){|event| state.backgroundcolor! event.target.value}}
-
+      div{input(type: :color).on(:change){|event| state.background_color! event.target.value}}
+      RadioInput(value: state.color, values: ['white', 'black'], name: 'color', on_change: lambda{|v| state.color! v})
       div{button{'Guardar'}.on(:click) do
-        hsh = {display: state.display, price: state.price, backgroundcolor: state.backgroundcolor, color: 'white'}
+        hsh = item
         $controller.update('item',  params.doc['id'], hsh)
-      end}
+      end} if ValidateItem.validate item
     end
   end
 
@@ -68,7 +73,7 @@ class GrouperAdministration < DisplayList
   before_mount do
     state.new_item! nil
     state.edit_item! nil
-    watch_ 'groupers', []
+    watch_ 'groupers'
   end
 
   def render
@@ -77,12 +82,12 @@ class GrouperAdministration < DisplayList
         StringInput(placeholder: 'display', value: state.new_item, on_change: lambda{|v| state.new_item! v})
         button{'Nuevo'}.on(:click) do
           slug = state.new_item.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-          hsh = {code: slug, type: 'grouper', display: state.new_item, price: 0.0, complements: []}
+          hsh = {code: slug, type: 'grouper', display: state.new_item}
           $controller.insert('item', hsh)
         end if state.new_item != ''
         state.docs.each do |doc|
-          div(key: doc['id'], style: {backgroundcolor: 'green', color: 'white'}) do
-            span{doc['display']}
+          div(key: doc['id']) do
+            span(style: {'backgroundColor' => 'green', 'color' => 'white'}){doc['display']}
             a(href: '#'){'editar'}.on(:click){state.edit_item! doc['code']}
           end
           EditGrouper(doc: doc) if state.edit_item == doc['code']
@@ -99,7 +104,7 @@ class ItemAdministration < DisplayList
     state.edit_item! nil
     state.search! ''
     @search = RVar.new ''
-    watch_ 'items_by_pattern', @search, [@search]
+    watch_ 'items_by_pattern', @search #, [@search]
   end
 
   def item
@@ -120,12 +125,26 @@ class ItemAdministration < DisplayList
       end if ValidateItem.validate item
       StringInput(placeholder: 'búsqueda', value: state.search, on_change: lambda{|v| @search.value = v; state.search! v})
       state.docs.each do |doc|
-        div(key: doc['id'], style: {backgroundcolor: doc['backgroundcolor'], color: doc['color']}) do
-          span{doc['display']}
+        div(key: doc['id']) do
+          span(style: {'backgroundColor' => doc['background-color'], 'color' => doc['color']}){doc['display']}
           a(href: '#'){'editar'}.on(:click){state.edit_item! doc['code']}
         end
         EditItem(doc: doc) if state.edit_item == doc['code']
       end #if state.search.length >= 2
+    end
+  end
+end
+
+class ItemInput < React::Component::Base
+  before_mount do
+    $controller.rpc('items').then do |docs|
+      print docs
+    end
+  end
+
+  def render
+    div do
+      'Item Input'
     end
   end
 end
@@ -176,6 +195,7 @@ class Administration < React::Component::Base
     div(class: 'flex-container') do
       ItemAdministration()
       GrouperAdministration()
+      ItemInput()
     end
   end
 end
